b94658fa4838e73d4065beaee23fd08d6648dfc0
MAGIC <tianzhonghiroki82@gmail.com>
2025-10-15 02:40:11 +0900

    update

--- api/webhook.js
+++ api/webhook.js
@@ -3,43 +3,40 @@ import { json } from 'micro';
 
 
 // linebotSDK?を初期化
-try{
-  const config = {
-    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_CHANNEL_SECRET
-  };
-  const client = new Client(config);
-}catch{
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+const client = new Client(config);
 
-}
 
 
 
 
 export default async function handler(req, res) {
 
-  const body = await json(req);
-  const events = body.events || [];
+  // const body = await json(req);
+  // const events = body.events || [];
 
-  // 受信イベントごとに処理
-  await Promise.all(
-    events.map(async (event) => {
-      if (event.type === 'message' && event.message.type === 'text') {
-        const replyToken = event.replyToken;
-        const userMessage = event.message.text;
+  // // 受信イベントごとに処理
+  // await Promise.all(
+  //   events.map(async (event) => {
+  //     if (event.type === 'message' && event.message.type === 'text') {
+  //       const replyToken = event.replyToken;
+  //       const userMessage = event.message.text;
 
-        // オウム返し
-        try {
-          await client.replyMessage(replyToken, {
-            type: 'text',
-            text: userMessage,
-          });
-        } catch (err) {
-          console.error('Reply failed:', err);
-        }
-      }
-    })
-  );
+  //       // オウム返し
+  //       try {
+  //         await client.replyMessage(replyToken, {
+  //           type: 'text',
+  //           text: userMessage,
+  //         });
+  //       } catch (err) {
+  //         console.error('Reply failed:', err);
+  //       }
+  //     }
+  //   })
+  // );
 
   res.status(200).send('OK');
 }
