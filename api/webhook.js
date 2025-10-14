ec08b6e50e2fe669f432a26e3890ed625ac8aa46
MAGIC <tianzhonghiroki82@gmail.com>
2025-10-15 02:35:16 +0900

    update

--- api/webhook.js
+++ api/webhook.js
@@ -12,12 +12,6 @@ const client = new Client(config);
 
 
 export default async function handler(req, res) {
-  // POST以外はじく
-  if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
-    return;
-  }
-
 
   const body = await json(req);
   const events = body.events || [];
