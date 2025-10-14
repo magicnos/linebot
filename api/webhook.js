5773ddc54e67dd579f8e091745d776e0e7a6458a
MAGIC <tianzhonghiroki82@gmail.com>
2025-10-15 02:30:36 +0900

    update

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,9 @@
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+
 export default async function handler(req, res) {
   res.status(200).send('OK');
 }
+
